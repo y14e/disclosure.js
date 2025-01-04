fe2ff5c7220831de43c7c88c8c7b8b50dbee408a
Yusuke Kamiyamane <y14e@yusukekamiyamane.com>
2025-01-04 16:01:51 +0900

    20250104

--- disclosure.js
+++ disclosure.js
@@ -72,15 +72,7 @@ class Disclosure {
     event.preventDefault();
     const index = [...this.summaries].indexOf(document.activeElement);
     const length = this.summaries.length;
-    this.summaries[
-      key === 'ArrowUp' ?
-        index - 1 < 0 ?
-          length - 1
-        : index - 1
-      : key === 'ArrowDown' ? (index + 1) % length
-      : key === 'Home' ? 0
-      : length - 1
-    ].focus();
+    this.summaries[key === 'ArrowUp' ? (index - 1 < 0 ? length - 1 : index - 1) : key === 'ArrowDown' ? (index + 1) % length : key === 'Home' ? 0 : length - 1].focus();
   }
 }
 
