47acdc5bc2b4818059f1ac1da53340cb9d9d3e39
Yusuke Kamiyamane <y@y14e.com>
2025-01-26 21:54:49 +0900

    20250126

--- disclosure.ts
+++ disclosure.ts
@@ -38,7 +38,7 @@ class Disclosure {
     const summary = details.querySelector('summary') as HTMLElement;
     const content = summary.nextElementSibling as HTMLElement;
     const height = `${content.scrollHeight}px`;
-    content.addEventListener('transitionend', function _once(e) {
+    content.addEventListener('transitionend', function handleTransitionEnd(e) {
       if (e.propertyName !== 'max-height') {
         return;
       }
@@ -51,7 +51,7 @@ class Disclosure {
         delete details.dataset.disclosureClosing;
       }
       content.style.maxHeight = content.style.overflow = '';
-      this.removeEventListener('transitionend', _once);
+      this.removeEventListener('transitionend', handleTransitionEnd);
     });
     content.style.maxHeight = isOpen ? '0' : height;
     content.style.overflow = 'clip';
